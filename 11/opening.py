f6a47bb76f2d7c4295da45e07da67696788c4a02
Vadym271 <limfocid@gmail.com>
2026-01-13 21:46:27 +0200

    Delete 11 directory
    it is not supposed to be here

--- 11/opening.py
+++
@@ -1,8 +0,0 @@
-with open('myfile.txt') as f:
-    line = f.read()
-print(line)
-
-#Note 1: the file does show up in a directory where i ran scripts
-#Note 2: since different directory does not contain our file python won't find the file
-# if we add a different directory to path of the file
-
